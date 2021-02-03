c6e335806f5c672269a6a9ce40e9ec18c2d53254
bimbraw <bimbrawkeshav@gmail.com>
2021-02-03 17:17:42 -0500

    Made separate files

---
+++ homework1_2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def linear_regression (X_tr, y_tr):
+    ...
+
+def train_age_regressor ():
+    # Load data
+    X_tr = np.reshape(np.load("age_regression_Xtr.npy"), (-1, 48*48))
+    ytr = np.load("age_regression_ytr.npy")
+    X_te = np.reshape(np.load("age_regression_Xte.npy"), (-1, 48*48))
+    yte = np.load("age_regression_yte.npy")
+
+    w = linear_regression(X_tr, ytr)
+
+    # Report fMSE cost on the training and testing data (separately)
+    # ...
